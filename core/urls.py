118b5794bc587cf0da67e2d76ca14bf068c6041e
Fjinete1-tech <fjinete1@gmail.com>
2026-07-09 22:53:45 -0500

    actualización

--- core/urls.py
+++ core/urls.py
@@ -3,8 +3,13 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path
 
+from .views import HomeView, AboutView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', HomeView.as_view(), name='home'),
+    path('about/', AboutView.as_view(), name='about'),
+    
 ]
 
 if settings.DEBUG:
